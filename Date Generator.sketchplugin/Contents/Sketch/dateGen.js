8f32c4b78f4de0e564b5fef58d9a900c758aa329
administrador <bernardo@Bernardos-MacBook-Pro.local>
2017-07-17 04:01:06 -0400

    fixed year stuck in the 70s

--- Date Generator.sketchplugin/Contents/Sketch/dateGen.js
+++ Date Generator.sketchplugin/Contents/Sketch/dateGen.js
@@ -9,42 +9,22 @@ if (selectedCount == 0) {
 	else { 
 	for (var i = 0; i < selectedCount; i++) {
         var layer = selectedLayers[i];
-        log(layer);
-		var start=new Date(2014, 0, 1);
-		var end=new Date();
+	
+		var startD = new Date(2012, 0, 1);
+		var endD = new Date();
+		
 		var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        var field_a = start.getTime();
-        var field_b = end.getTime();
         
-        var end_start = field_b - field_a;    
-        var randomized = Math.random() * end_start;
-        log('field a log start gettime = ' + field_a);
-        log('field b log end gettime = ' + field_b);
-        log('randomized = ' + randomized);
-
-        randomNum = field_a + randomized;
-        log('randomDate = ' + randomNum);
-        
-       	/*offset_e = ("2012-01-01 00:00:00").getTime();
-		end_e = ("2013-01-01 00:00:00").getTime();
-		diff_e = end_e - offset_e + 1;
-		
-		rand_e = (offset_e + (Math.random() * diff_e));
-		var randomNum = new Date(rand_e);
-		*/
-		
-		
-        var randomNum = new Date(randomized);
-        
+        randomNum =  (new Date(startD.getTime() + Math.random() * (endD.getTime() - startD.getTime())));
         var dia = randomNum.getDate();
 		if (dia.toString().length == 1) {
 			dia = "0"+dia;
 		}
-        
         var mes = randomNum.getMonth()+1
-		var ano = randomNum.getFullYear() 
+		var ano = randomNum.getFullYear();
+		
 		var finalText = "";
-				
+			
 		switch (myCase)
 
  		{
@@ -95,9 +75,9 @@ if (selectedCount == 0) {
           		mes = mes;
 				break;
 
- }
+ 		}
 		
-		layer.setStringValue(finalText);
+		layer.setStringValue(finalText+"");
 
 		}
 	}
